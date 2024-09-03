0ad099178bb66116d3e1fb2ed1a7bff6f60a84b9
Admin <reteamboss@gmail.com>
2024-09-03 20:15:22 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    var array = arrayOf(1,2,3,4,5,6,7,8,9)
+    var isEven = fun (a: Int) = a % 2 == 0
+
+
+
+    fun checkArrayElement (array: Array<Int>, isEven: (Int) -> Boolean): ArrayList<Boolean> {
+        var result: ArrayList<Boolean> = arrayListOf()
+        for (i in array){
+            result.add(isEven(i))
+        }
+        return result
+    }
+
+    println(checkArrayElement(array, isEven))
 }
